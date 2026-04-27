44bc0d781949edc35dc5cfb2bac72d3652844ac0
Karunya Varsha <karunyavarsha2004_bec28@mepcoeng.ac.in>
2026-04-27 11:04:43 +0530

    Add files via upload

---
+++ common.py
@@ -0,0 +1,7 @@
+#EXPERIMENT 3
+#PROGRAMMER:S.RITHIKHA
+#6.COMMON CHARACTERS USING SET OPERATION
+s1=input("enter first string:")
+s2=input("enter second string:")
+c=set(s1)&set(s2)
+print("common characters:",c)
