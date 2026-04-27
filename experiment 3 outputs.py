fd73b6e0c29bbbc066e56212f7cb74b5937265b3
Karunya Varsha <karunyavarsha2004_bec28@mepcoeng.ac.in>
2026-04-27 11:05:29 +0530

    Add files via upload

---
+++ experiment 3 outputs.py
@@ -0,0 +1,46 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\Administrator\Desktop\24bec139\experiment 3\vowels.py ===
+enter a string:rithikha
+the no. of vowels in the string is: 3
+
+== RESTART: C:\Users\Administrator\Desktop\24bec139\experiment 3\palindrome.py =
+enter the string:malayalam
+the given string is a palindrome
+>>> 
+== RESTART: C:\Users\Administrator\Desktop\24bec139\experiment 3\duplicate.py ==
+enter the string:rithikha
+after removing duplicates: rithka
+>>> 
+== RESTART: C:\Users\Administrator\Desktop\24bec139\experiment 3\frequency.py ==
+enter a stringhello world
+h : 1
+e : 1
+l : 3
+l : 3
+o : 2
+  : 1
+w : 1
+o : 2
+r : 1
+l : 3
+d : 1
+>>> 
+= RESTART: C:\Users\Administrator\Desktop\24bec139\experiment 3\reverse words.py
+enter a sentencehello world
+ollehdlrow
+>>> 
+==== RESTART: C:\Users\Administrator\Desktop\24bec139\experiment 3\common.py ===
+enter first string:hi
+enter second string:hello
+common characters: {'h'}
+>>> 
+==== RESTART: C:\Users\Administrator\Desktop\24bec139\experiment 3\unique.py ===
+enter the string:rithikha
+unique characters: {'a', 'i', 't', 'r', 'k', 'h'}
+>>> 
+= RESTART: C:\Users\Administrator\Desktop\24bec139\experiment 3\present in one.py
+enter first stringhello
+enter second stringhi
+characters only in first string: {'l', 'o', 'e'}
