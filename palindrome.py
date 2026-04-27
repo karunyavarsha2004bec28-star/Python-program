4a8c843c3a759294c21cc9241109f1d582bc1c88
Karunya Varsha <karunyavarsha2004_bec28@mepcoeng.ac.in>
2026-04-27 11:06:09 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+#EXPERIMENT 3
+#PROGRAMMER:S.RITHIKHA
+#2.TO CHECK PALINDROME
+s=input("enter the string:")
+if s==s[::-1]:
+    print("the given string is a palindrome")
+else:
+    print("the given string is not a palindrome")
